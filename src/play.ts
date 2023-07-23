e69323a89bd6ce41ddd8fe96b0a5bfe2c9b678b3
Ted Shaffer <shaffer.family@gmail.com>
2023-07-23 06:36:19 -0700

    First step in creating appropriate prompt.

--- src/play.ts
+++ src/play.ts
@@ -1,25 +1,35 @@
-  //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
-  import { OpenAI } from "langchain/llms";
+//Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
+import { OpenAI } from "langchain/llms";
 
-  //Load environment variables (populate process.env from .env file)
-  import * as dotenv from "dotenv";
-  dotenv.config();
+//Load environment variables (populate process.env from .env file)
+import * as dotenv from "dotenv";
+dotenv.config();
 
-  export const run = async () => {
+export const run = async () => {
 
-      //Instantiante the OpenAI model 
-      //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
-      const model = new OpenAI({ temperature: 0 });
+    //Instantiante the OpenAI model 
+    //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
+    const model = new OpenAI({ temperature: 0 });
 
-      const input = `
-      Given an input question, answer the question and return the answer using the following format:
-      Question: Question here
-      Answer: Answer here
+    // const input = `
+    //   Given an input question, answer the question and return the answer using the following format:
+    //   Question: Question here
+    //   Answer: Answer here
 
-      What is the capital of France?
-          `;
+    //   What is the capital of France?
+    //       `;
 
-      //Calls out to the model's (OpenAI's) endpoint passing the prompt. This call returns a string
-      const res = await model.call(input);
-      console.log({ res });
-  };
+    const input = `
+        The user will provide input. The input will include the following: a command to display photos; list of people whose photos should be displayed;
+        under what conditions the photos should be selected.
+
+        Parse the input and respond with the following format:
+            Command: command here
+            List: the list of people whose photos should be displayed
+            Conditions: the conditions under which the photos should be selected.
+          
+        Display photos of Sam from the year 1992.
+              `;
+    const res = await model.call(input);
+    console.log({ res });
+};
