16d3248025ec747fdf041cfdd46dc022a35f7f04
Ted Shaffer <shaffer.family@gmail.com>
2023-07-23 07:00:32 -0700

    another iteration

--- src/play.ts
+++ src/play.ts
@@ -1,23 +1,27 @@
-//Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
-import { OpenAI } from "langchain/llms";
+import { OpenAI } from "langchain/llms/openai";
 
-//Load environment variables (populate process.env from .env file)
 import * as dotenv from "dotenv";
 dotenv.config();
 
 export const run = async () => {
 
-    //Instantiante the OpenAI model 
-    //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
     const model = new OpenAI({ temperature: 0 });
 
-    // const input = `
-    //   Given an input question, answer the question and return the answer using the following format:
-    //   Question: Question here
-    //   Answer: Answer here
+    // list of prior prompts
 
-    //   What is the capital of France?
-    //       `;
+    // Display photos of Sam from the year 1992.
+
+    // Display photos of Sam and Joel from the years 1990 - 1992.
+    // res: '\n' +
+    // '        Command: Display photos\n' +
+    // '        List: Sam, Joel\n' +
+    // '        Conditions: Years 1990 - 1992'
+
+    // Display photos of both Sam and Joel but not Rachel from the years 1990 - 1992.
+    // res: '\n' +
+    // '        Command: Display photos\n' +
+    // '        List: Sam, Joel, Rachel\n' +
+    // '        Conditions: Years 1990 - 1992, exclude Rachel'
 
     const input = `
         The user will provide input. The input will include the following: a command to display photos; list of people whose photos should be displayed;
@@ -28,7 +32,7 @@ export const run = async () => {
             List: the list of people whose photos should be displayed
             Conditions: the conditions under which the photos should be selected.
           
-        Display photos of Sam from the year 1992.
+        Display photos of both Sam and Joel but not Rachel from the years 1990 - 1992.
               `;
     const res = await model.call(input);
     console.log({ res });
