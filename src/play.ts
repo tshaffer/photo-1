c6342cbb24f227938ea6140403f2e21346477b4f
Ted Shaffer <shaffer.family@gmail.com>
2023-07-24 06:11:47 -0700

    more tag testing

--- src/play.ts
+++ src/play.ts
@@ -31,6 +31,8 @@ export const run = async () => {
     // '        List: European vacations\n' +
     // '        Conditions: None'
 
+    // Display photos of bears and moose from our Glacier vacation.
+
     const input = `
         The user will provide input. The input will include the following: a command to display photos; a specification of which photos to display;
         under what conditions the photos should be displayed.
@@ -60,7 +62,13 @@ export const run = async () => {
             Conditions: Years 1990 - 1992
         
         If there is a specification of which photos to display, then each item in the specification must be in the photos tag list.
-        The photos tag list includes the following items: Sam, Joel, and Rachel.
+        The photos tag list includes the following items: 
+            Sam
+            Joel
+            Rachel
+            Moose
+            Bear
+
         If an item in the specification is not in the photos tag list, then immediately respond with the following:
         Error - item not in tag list: the item that is not in the specification.
 
@@ -69,7 +77,7 @@ export const run = async () => {
         List: the specification of which photos to display
         Conditions: the conditions under which the photos should be selected.
     
-        Display photos of Joel and Sam and Noah from the year 1993.
+        Display photos of bears and moose from our 2023 Glacier vacation.
     `;
     
     const res = await model.call(input);
