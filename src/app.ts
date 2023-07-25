5f36cc4ed13dc2563be0eb2fd6e1224a66a869f4
Ted Shaffer <shaffer.family@gmail.com>
2023-07-25 11:36:08 -0700

    initial use of photo related template with an output parser.

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,23 @@ import { StructuredOutputParser } from "langchain/output_parsers";
 
 // With a `StructuredOutputParser` we can define a schema for the output.
 const parser = StructuredOutputParser.fromNamesAndDescriptions({
-    cityAnswer: "answer to the user's question about the capital",
-    continentAnswer: "answer to the user's question about the continent",
+    photosToDisplayAnswer: "answer for photos to display",
+    photoDatesAnswer: "answer for the photo dates",
 });
 
 const formatInstructions = parser.getFormatInstructions();
 
 // const template = 'Answer the users question as best as possible.\n{format_instructions}\n{question}';
 const template = `
-    Answer the users question as best as possible.    
+    The user will provide input. The input will include the following: a command to display photos; which photos to display;
+    the dates for the photos.
+
+    Example input: Display photos of either Sam or Joel from the years 1990 - 1992
+    The answer for photos to display is: Sam or Joel
+    The answer for the photo dates is: 1990 - 1992
+    
+    Answer the users question as best as possible. 
+    
     {format_instructions}\n{question}
 `;
 
@@ -35,11 +43,11 @@ async function main() {
     const model = new OpenAI({ temperature: 0 });
 
     const input = await prompt.format({
-        question: "What is the capital of France and what continent is it in?",
+        question: "Display photos of either Sam or Joel from the years 1990 - 1992",
     });
     const response = await model.call(input);
 
-    // console.log(input);
+    console.log(input);
 
     console.log(response);
 
