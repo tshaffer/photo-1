635e85f840a6cdf9f902ae7595830a286fe5cfce
Ted Shaffer <shaffer.family@gmail.com>
2023-07-25 08:28:57 -0700

    in progress failblog.

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import { getDbData } from "./controllers";
 import { exit } from "process";
 import { OpenAI } from "langchain/llms/openai";
 
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import { PromptTemplate } from "langchain/prompts";
+
 dotenv.config();
 
 async function main() {
@@ -14,8 +17,6 @@ async function main() {
     // await connectDB();
 
     // const dbData: DbData = await getDbData();
-
-    const model = new OpenAI({ temperature: 0 });
 
     const input = `
   The user will provide input. The input will include the following: a command to display photos; a specification of which photos to display;
@@ -30,34 +31,25 @@ async function main() {
   Error - conditions not specified correctly.
 
   Otherwise, parse the input and respond with the following format:
-  Command: command here
   List: the specification of which photos to display. The specification must be a logical expression as described in the examples below for List.
   Conditions: the specification of the conditions under which the photos should be selected. This specification must be a logical expression that should
   include tags, date ranges, and any appropriate logic operators.
 
   Example input: Display photos of either Sam or Joel from the years 1990 - 1992
   For this input, the output should be as follows:
-      Command: Display photos
-      List: (Sam || Joel)
-      Conditions: Date::years(1990 - 1992)
+      \{ List:ist: (Sam || Joel), Conditions: Date::years(1990 - 1992) \}
       
   Example input: Display photos of both Sam and Joel from the years 1990 - 1992
   For this input, the output should be as follows:
-      Command: Display photos
-      List: (Sam && Joel)
-      Conditions: Date::years(1990 - 1992)
+      \{ List:ist: (Sam && Joel), Conditions: Date::years(1990 - 1992) \}
       
   Example input: Display photos of both Sam and Joel but not Rachel from the years 1990 - 1992
   For this input, the output should be as follows:
-      Command: Display photos
-      List: ((Sam && Joel) && !Rachel)
-      Conditions: Date::years(1990 - 1992)
+      \{ List:ist: ((Sam && Joel) && !Rachel), Conditions: Date::years(1990 - 1992) \}
       
   Example input: Display photos of both Sam or Joel but not Rachel from the years 1990 - 1992
   For this input, the output should be as follows:
-      Command: Display photos
-      List: ((Sam || Joel) && !Rachel)
-      Conditions: Date::years(1990 - 1992)
+      \{ List:ist: ((Sam || Joel) && !Rachel), Conditions: Date::years(1990 - 1992) \}
   
   If there is a specification of which photos to display, then each item in the specification must be in the photos tag list.
   The photos tag list includes the following items: 
@@ -68,13 +60,35 @@ async function main() {
       Bear
       Vacations
 
-      Display photos of either Sam or Joel from the years 1990 - 1992
+  {command}
 `;
 
-    // Display photos of Sam and Joel from 1991 - 1993.
+// Display photos of either Sam or Joel from the years 1990 - 1992
+// Display photos of Sam and Joel from 1991 - 1993.
 
-    const res = await model.call(input);
+    const parser = StructuredOutputParser.fromNamesAndDescriptions({
+        List: 'the list as described above',
+        Conditions: 'the conditions as described above'
+    });
+    const formatInstructions = parser.getFormatInstructions();
+    const prompt = new PromptTemplate({
+        template: input,
+        inputVariables: ['command'],
+        partialVariables: { formatInstructions },
+    });
+
+
+    const model = new OpenAI({ temperature: 0 });
+
+    const modelInput = await prompt.format({
+        command: 'Display photos of either Sam or Joel from the years 1990 - 1992'
+    })
+
+    const res = await model.call(modelInput);
     console.log({ res });
+
+    const parsedResponse = await parser.parse(res);
+    console.log(parsedResponse);
 
     console.log('exit');
 
