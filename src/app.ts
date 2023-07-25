3b4c6b3adfc5ffcf7067ae910874bd2c841d488a
Ted Shaffer <shaffer.family@gmail.com>
2023-07-25 14:13:23 -0700

    Another example.

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,6 @@ const parser = StructuredOutputParser.fromNamesAndDescriptions({
 
 const formatInstructions = parser.getFormatInstructions();
 
-// const template = 'Answer the users question as best as possible.\n{format_instructions}\n{question}';
 const template = `
     The user will provide input. The input will include the following: a command to display photos; a spec for the photos to display.
     The spec for which photos to display must includes tags (from the list of tags below) and/or a date specification.
@@ -26,10 +25,13 @@ const template = `
     Rachel
     Moose
     Bear
-    Vacations
+    Vacation
 
     Example input: Display photos of either Sam or Joel from the years 1990 - 1992
     The spec for the photos to display is: (Sam or Joel) && dates(1990-1992)
+        
+    Example input: Display photos of either Sam and Joel from the our vacation.
+    The spec for the photos to display is: (Sam and Joel) && Vacations
         
     {format_instructions}\n{command}
 `;
@@ -47,7 +49,7 @@ async function main() {
     const model = new OpenAI({ temperature: 0 });
 
     const input = await prompt.format({
-        command: "Display photos of either Sam or Joel from the years 1990 - 1992",
+        command: "Display photos of either Sam or Rachel or Ted from our vacation",
     });
     const response = await model.call(input);
 
