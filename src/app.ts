ed3c71967b9cf2a6503a4aeb95a3027c6460dcd1
Ted Shaffer <shaffer.family@gmail.com>
2023-07-25 11:16:39 -0700

    Successfully used StructuredOutputParser with latest code.

--- src/app.ts
+++ src/app.ts
@@ -4,81 +4,42 @@ import { DbData, MediaItem } from "entities";
 import { getDbData } from "./controllers";
 import { exit } from "process";
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { StructuredOutputParser } from "langchain/output_parsers";
+
+
+// With a `StructuredOutputParser` we can define a schema for the output.
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+    cityAnswer: "answer to the user's question about the capital",
+    continentAnswer: "answer to the user's question about the continent",
+});
+
+const formatInstructions = parser.getFormatInstructions();
+
+const prompt = new PromptTemplate({
+    template:
+        "Answer the users question as best as possible.\n{format_instructions}\n{question}",
+    inputVariables: ["question"],
+    partialVariables: { format_instructions: formatInstructions },
+});
 
 dotenv.config();
 
 async function main() {
 
-    console.log('main invoked');
-
-    // await connectDB();
-
-    // const dbData: DbData = await getDbData();
-
     const model = new OpenAI({ temperature: 0 });
 
-    const input = `
-  The user will provide input. The input will include the following: a command to display photos; a specification of which photos to display;
-  under what conditions the photos should be displayed.
+    const input = await prompt.format({
+        question: "What is the capital of France and what continent is it in?",
+    });
+    const response = await model.call(input);
 
-  If an item in the specification is not in the photos tag list, then immediately respond with only the following:
-  Error - item not in tag list: the item that is not in the specification.
+    // console.log(input);
 
-  The conditions under which conditions the photos should be displayed can only include a date specification, a date range specification, tags 
-  in the tag list, or a combination of these items.
-  If the conditions under which conditions the photos should be displayed do not meet this criteria, then immediately respond with the following:
-  Error - conditions not specified correctly.
+    console.log(response);
 
-  Otherwise, parse the input and respond with the following format:
-  Command: command here
-  List: the specification of which photos to display. The specification must be a logical expression as described in the examples below for List.
-  Conditions: the specification of the conditions under which the photos should be selected. This specification must be a logical expression that should
-  include tags, date ranges, and any appropriate logic operators.
-
-  Example input: Display photos of either Sam or Joel from the years 1990 - 1992
-  For this input, the output should be as follows:
-      Command: Display photos
-      List: (Sam || Joel)
-      Conditions: Date::years(1990 - 1992)
-      
-  Example input: Display photos of both Sam and Joel from the years 1990 - 1992
-  For this input, the output should be as follows:
-      Command: Display photos
-      List: (Sam && Joel)
-      Conditions: Date::years(1990 - 1992)
-      
-  Example input: Display photos of both Sam and Joel but not Rachel from the years 1990 - 1992
-  For this input, the output should be as follows:
-      Command: Display photos
-      List: ((Sam && Joel) && !Rachel)
-      Conditions: Date::years(1990 - 1992)
-      
-  Example input: Display photos of both Sam or Joel but not Rachel from the years 1990 - 1992
-  For this input, the output should be as follows:
-      Command: Display photos
-      List: ((Sam || Joel) && !Rachel)
-      Conditions: Date::years(1990 - 1992)
-  
-  If there is a specification of which photos to display, then each item in the specification must be in the photos tag list.
-  The photos tag list includes the following items: 
-      Sam
-      Joel
-      Rachel
-      Moose
-      Bear
-      Vacations
-
-      Display photos of either Sam or Joel from the years 1990 - 1992
-`;
-
-    // Display photos of Sam and Joel from 1991 - 1993.
-
-    const res = await model.call(input);
-    console.log({ res });
-
-    console.log('exit');
-
-    exit();
-}
+    console.log(await parser.parse(response));
+};
 
 main();
+
