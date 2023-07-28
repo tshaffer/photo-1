586ef3e6b4f2ea687def99ace041b85024caf503
Ted Shaffer <shaffer.family@gmail.com>
2023-07-28 06:35:29 -0700

    success with LLM portion bypassed (used previous results of LLM call).

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import * as dotenv from "dotenv";
 import connectDB from './config/db';
-import { DbData, MediaItem } from "entities";
+import { DbData, MediaItem, TagToMediaItemsLUT } from "entities";
 import { getDbData } from "./controllers";
 import { exit } from "process";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
 import { StructuredOutputParser } from "langchain/output_parsers";
+import { cloneDeep, isNil } from 'lodash';
 
 
 // With a `StructuredOutputParser` we can define a schema for the output.
@@ -25,6 +26,8 @@ const template = `
     A single year is specified as: dateSpec**year**
 
     The list of tags includes: 
+    Ted
+    Lori
     Sam
     Joel
     Rachel
@@ -56,28 +59,118 @@ dotenv.config();
 
 async function main() {
 
-    const model = new OpenAI({ temperature: 0 });
+    console.log('main invoked');
 
-    const input = await prompt.format({
-        // command: "Display photos from our 2021 and 2023 vacations",                   // worked
-        command: "Display photos from vacations 2021-2023",                             // worked
-        // command: "Display photos from vacations from the years 2021 - 2023",        // worked
-    });
-    const response = await model.call(input);
+    await connectDB();
 
-    console.log(input);
+    const dbData: DbData = await getDbData();
 
-    console.log(response);
+    // const model = new OpenAI({ temperature: 0 });
 
-    const responseAsObject = await parser.parse(response);
-    console.log(responseAsObject);
+    // const input = await prompt.format({
+    //     // command: "Display photos from our 2021 and 2023 vacations",                   // worked
+    //     // command: "Display photos from vacations 2021-2023",                             // worked
+    //     // command: "Display photos from vacations from the years 2021 - 2023",        // worked
+    //     command: "Display photos of Joel or Lori"
+    // });
+    // const response = await model.call(input);
 
-    const photosToDisplaySpecStr = responseAsObject.photosToDisplaySpec;
+    // console.log('INPUT');
+    // console.log(input);
 
-    const tokens: string[] = photosToDisplaySpecStr.split('XXXX');
-    
+    // console.log('RESPONSE');
+    // console.log(response);
+
+    // console.log('PARSED RESPONSE');
+    // const responseAsObject = await parser.parse(response);
+    // console.log(responseAsObject);
+
+    // const matchingMediaItems: MediaItem[] = parsePhotosToDisplaySpec(dbData, responseAsObject.photosToDisplaySpec);
+
+    const matchingMediaItems: MediaItem[] = parsePhotosToDisplaySpec(dbData, 'JoelXXXXLoriXXXXOR');
+    console.log('matchingMediaItems');
+    console.log(matchingMediaItems);
+
     console.log('exit');
 };
 
+const isOperator = (token: string): boolean => {
+    switch (token.toLowerCase()) {
+        case 'and':
+        case 'or':
+        case 'not':
+            return true;
+    }
+    return false;
+}
+
+const applyOrs = (tags: string[], tagToMediaItemsLUT: TagToMediaItemsLUT): MediaItem[] => {
+
+    const resultingMediaItems: MediaItem[] = [];
+
+    for (const tag of tags) {
+
+        if (tagToMediaItemsLUT.hasOwnProperty(tag.toLowerCase())) {
+
+            // get mediaItems for this tag
+            const mediaItemsWithThisTag: MediaItem[] = tagToMediaItemsLUT[tag.toLowerCase()];
+
+            // TEDTODO - is there such a thing as a set in javascript??
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+            // merge with mediaItems from prior tags
+            for (const mediaItemWithThisTag of mediaItemsWithThisTag) {
+                const result = resultingMediaItems.find(({ googleId }) => googleId === mediaItemWithThisTag.googleId);
+                if (isNil(result)) {
+                    resultingMediaItems.push(mediaItemWithThisTag);
+                }
+            }
+        }
+    }
+
+    return resultingMediaItems;
+}
+
+const applyLogicalOperation = (dbData: DbData, mediaItems: MediaItem[], tags: string[], operator: string): MediaItem[] => {
+
+    switch (operator.toLowerCase()) {
+        case 'and':
+            return [];
+        case 'or':
+            return applyOrs(tags, dbData.tagToMediaItemsLUT);
+        case 'not':
+            return [];
+    }
+
+    return [];
+}
+
+function parsePhotosToDisplaySpec(dbData: DbData, photosToDisplaySpecStr: string): MediaItem[] {
+
+    const { mediaItems, tagToMediaItemsLUT } = dbData;
+    const tokens: string[] = photosToDisplaySpecStr.split('XXXX');
+
+    const tags: string[] = [];
+    let activeListOfMediaItems: MediaItem[] = cloneDeep(mediaItems);
+
+    for (const token of tokens) {
+        if (isOperator(token)) {
+            // apply logical operation on tags on stack, starting with activeListOfMediaItems => updated list of mediaItems
+            activeListOfMediaItems = applyLogicalOperation(dbData, activeListOfMediaItems, tags, token);
+        } else {
+            // token is a tag
+            if (!tagToMediaItemsLUT.hasOwnProperty(token.toLowerCase())) {
+                throw ('NotATag');
+            }
+            // const mediaItemsForTag: MediaItem[] = tagToMediaItemsLUT[token.toLowerCase()];
+            // stack.push(mediaItemsForTag);
+            tags.push(token);
+        }
+    }
+
+    return activeListOfMediaItems;
+}
+
 main();
+
 
