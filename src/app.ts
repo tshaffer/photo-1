2c585b19b937227f79b0d2afa634a41d3bbf3918
Ted Shaffer <shaffer.family@gmail.com>
2023-07-29 07:58:42 -0700

    exit on completion after properly waiting invoking await on queryPhotos

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import * as dotenv from "dotenv";
 import { queryPhotos } from "./tasks/queryPhotos";
+import { exit } from "process";
 
 async function main() {
 
   dotenv.config();
 
-  queryPhotos();
+  await queryPhotos();
   console.log('exit');
 
+  exit();
 };
 
 main();
