2e5346681d6d3870a8cb045d942fcf642495d2d8
Ted Shaffer <shaffer.family@gmail.com>
2023-07-29 07:29:44 -0700

    Change indentation

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,7 @@ export let connection: mongoose.Connection;
 async function connectDB() {
   console.log('uri is:');
   console.log(process.env.MONGO_URI);
-  connection = await mongoose.createConnection(process.env.MONGO_URI as string); 
+  connection = await mongoose.createConnection(process.env.MONGO_URI as string);
   console.log(`MongoDB new db connected`);
 
   mongoose.Promise = global.Promise;
--- src/models/MediaItem.ts
+++ src/models/MediaItem.ts
@@ -5,17 +5,17 @@ const Schema = mongoose.Schema;
 
 const MediaitemSchema = new Schema(
   {
-      googleId: {type: String, required: true, unique: true},
-      fileName: {type: String, required: true},
-      filePath: {type: String, default: ''},
-      productUrl: {type: String},
-      mimeType: {type: String},
-      creationTime: {type: Date},
-      width: {type: Number},
-      height: {type: Number},
-      orientation: {type: Number, default: 0},
-      description: {type: String, default: ''},
-      gpsPosition: {type: String, default: ''}
+    googleId: { type: String, required: true, unique: true },
+    fileName: { type: String, required: true },
+    filePath: { type: String, default: '' },
+    productUrl: { type: String },
+    mimeType: { type: String },
+    creationTime: { type: Date },
+    width: { type: Number },
+    height: { type: Number },
+    orientation: { type: Number, default: 0 },
+    description: { type: String, default: '' },
+    gpsPosition: { type: String, default: '' }
   }
 );
 
